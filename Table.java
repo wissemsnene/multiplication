1f8292defa5d181527c1eff35f976b25638fe975
wissem <wissem.snene@gmail.com>
2017-10-10 15:22:07 +0100

    added multiplication program

---
+++ Table.java
@@ -0,0 +1,22 @@
+
+public class Table {
+
+	public static void main(String[] args) {
+
+			for (int j=2 ; j<=10; j++){
+				
+				System.out.println(" LA TABLE DE MULTIPLICATIN DE " +j);
+				
+				for (int i = 1 ; i <= 10; i++)
+				{
+					
+					System.out.println( j + " multiplier par " + i + " vaut " + i*j);
+				
+				
+			    }
+
+		}
+
+	}
+
+}
